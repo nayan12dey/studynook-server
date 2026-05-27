40dffda71960a419a26a03e960bebc8981d27d7a
Nayan Dey <ndeynayan123@gmail.com>
2026-05-27 13:20:52 +0530

    secure single room route using verify token middleware

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const dotenv = require("dotenv")
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const cors = require("cors")
+const cors = require("cors");
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 dotenv.config()
 
 const app = express()
@@ -13,6 +14,13 @@ const port = process.env.PORT
 
 
 const uri = process.env.MONGODB_URI
+
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`))
+
+console.log("JWKS", JWKS)
+
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,73 +37,124 @@ const logger = (req, res, next) => {
   next()
 }
 
+const verifyToken = async (req, res, next) => {
+
+  const { authorization } = req.headers
+  // console.log(req.headers)
+
+  const token = authorization?.split(" ")[1]
+  // console.log(token)
 
 
 
-
-
-
-  async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      // await client.db("admin").command({ ping: 1 });
-
-      const db = client.db("studynookdb")
-      const roomsCollection = db.collection("rooms")
-
-      app.get("/rooms", async (req, res) => {
-        const cursor = roomsCollection.find()
-        const result = await cursor.toArray()
-        // console.log(result)
-        res.send(result)
-      })
-
-
-      app.get("/availablerooms", async (req, res) => {
-        const cursor = roomsCollection.find().sort({ _id: -1 }).limit(6)
-        const result = await cursor.toArray()
-        res.send(result)
-      })
-
-
-      app.get("/rooms/:roomsId", logger, async (req, res) => {
-
-        const { roomsId } = req.params;
-
-        const query = { _id: new ObjectId(roomsId) }
-        const result = await roomsCollection.findOne(query)
-        res.send(result);
-      })
-
-
-
-
-
-
-
-
-
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      // await client.close();
-    }
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" })
   }
-  run().catch(console.dir);
+
+  try {
+    const JWKS = createRemoteJWKSet(
+      new URL('http://localhost:3000/api/auth/jwks')
+    )
+    const { payload } = await jwtVerify(token, JWKS)
+    req.user = payload;
+
+
+
+    next()
+
+  } catch (error) {
+    console.error('Token validation failed:', error)
+    return res.status(401).json({ message: "Unauthorized" })
+  }
+
+
+
+}
 
 
 
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
 
 
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    const db = client.db("studynookdb")
+    const roomsCollection = db.collection("rooms")
+
+    app.get("/rooms", async (req, res) => {
+
+      const { search } = req.query;
+
+      let cursor
+      if (search) {
+        cursor = roomsCollection.find({ room_name: search })
+      }
+      else {
+        cursor = roomsCollection.find()
+
+      }
+
+
+      const result = await cursor.toArray()
+      console.log(result)
+
+
+      // console.log(result)
+      res.send(result)
+    })
+
+
+    app.get("/availablerooms", async (req, res) => {
+      const cursor = roomsCollection.find().sort({ _id: -1 }).limit(6)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+
+    app.get("/rooms/:roomsId", logger, verifyToken, async (req, res) => {
+
+      console.log(req.user, "req")
+
+      const { roomsId } = req.params;
+
+      const query = { _id: new ObjectId(roomsId) }
+      const result = await roomsCollection.findOne(query)
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 
